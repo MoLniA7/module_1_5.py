d3c8cd390dd1c1a4760213da4050ecdca3f7fd55
MoLniA7 <vmacnev.vlad.macnev@gmail.com>
2024-09-09 17:51:50 +0800

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = 11, True, 'apple', 99
+print(immutable_var)
+mutable_list = [34, 'road', False, 1]
+print(mutable_list)
+mutable_list[1] = 'wood'
+mutable_list[3] = 'water'
+print(mutable_list)
